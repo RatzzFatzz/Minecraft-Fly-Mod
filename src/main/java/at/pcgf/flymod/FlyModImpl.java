c711467cde14cfc9336ba6193b126194d458e312
RatzzFatzz <michael@ratzloeffel.de>
2021-05-06 18:55:28 +0200

    Update key binding registration & make it use language files

--- src/main/java/at/pcgf/flymod/FlyModImpl.java
+++ src/main/java/at/pcgf/flymod/FlyModImpl.java
@@ -14,38 +14,34 @@
 
 package at.pcgf.flymod;
 
-import at.pcgf.flymod.gui.FlyModConfig;
-import at.pcgf.flymod.gui.FlyModConfigManager;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class FlyModImpl implements ClientModInitializer {
-    public static FabricKeyBinding flyKey = FabricKeyBinding.Builder.create(
-            new Identifier("flykey"),
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_B,
-            "key.categories.flymod"
-    ).build();
-
-
     public static byte flying = -1;
     public static final String MOD_ID = "flymod";
+    private static final KeyBinding flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.flymod.toggle",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_B,
+            "key.flymod.keybinding"
+    ));
 
     @Override
     public void onInitializeClient() {
-        FlyModConfig config = FlyModConfigManager.init();
-        KeyBindingRegistry.INSTANCE.register(flyKey);
-        ClientTickCallback.EVENT.register(e -> {
-            if(flyKey.wasPressed()){
-                System.out.println("b is pressed");
-                flying = (byte) (flying > 0 ? 0 : 1);
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if(flyKey.wasPressed()) {
+                flying = (byte) (flying > 0 ? 0: 1);
             }
         });
-
     }
+
+
+
+
+
 }
